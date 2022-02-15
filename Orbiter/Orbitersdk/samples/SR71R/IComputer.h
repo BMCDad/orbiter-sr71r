196fb729f91cd70e8a342ed632aff74837fcd588
Blake Christensen <bmcdad.christensen@gmail.com>
2022-02-15 14:01:03 -0700

    Fix flight computer key bug.

--- Orbiter/Orbitersdk/samples/SR71R/IComputer.h
+++ Orbiter/Orbitersdk/samples/SR71R/IComputer.h
@@ -20,48 +20,28 @@
 
 namespace FC // Flight Computer
 {
-    enum GCKey
-    {
-        D0, D1, D2, D3, D4, D5, D6, D7, D8, D9,
-        Decimal,
-        PlusMinus,
-        Clear,
-        Enter,
-        Previous, Next,
-        F1, F2, F3, F4, F5, F6, F7, F8, F9, F10,
-		Home
-    };
 
-	enum FCProg
-	{
-		Main,
-		Ascent,
-		Orbit,
-		ReEntry,
-		Atmosphere
-	};
+ //   /**
+ //   Define the interface the flight computer exposes to the programming layer.
+ //   */
+ //   class IComputer
+ //   {
+ //   public:
+ //       virtual void        ClearScreen() = 0;
+ //       virtual void        DisplayLine(int row, char* mask, ...) = 0;
+	//	virtual void		DisplayText(int row, int col, const char* text) = 0;
+ //       virtual int         DisplayCols() = 0;
+ //       virtual int         DisplayRows() = 0;
+	//	virtual double		GetScratchPad() = 0;
+	//	virtual void		SetScratchPad(double value) = 0;
+ //       virtual IAvionics*  Avionics() = 0;
+	//	virtual void		ActivateProg(FCProg prog) = 0;
+ //   };
 
-    /**
-    Define the interface the flight computer exposes to the programming layer.
-    */
-    class IComputer
-    {
-    public:
-        virtual void        ClearScreen() = 0;
-        virtual void        DisplayLine(int row, char* mask, ...) = 0;
-		virtual void		DisplayText(int row, int col, const char* text) = 0;
-        virtual int         DisplayCols() = 0;
-        virtual int         DisplayRows() = 0;
-		virtual double		GetScratchPad() = 0;
-		virtual void		SetScratchPad(double value) = 0;
-        virtual IAvionics*  Avionics() = 0;
-		virtual void		ActivateProg(FCProg prog) = 0;
-    };
-
-	class IDisplay
-	{
-	public:
-		virtual void		SetFuncLabel(GCKey key, const char* label) = 0;
-		virtual void		DisplayText(int row, int col, const char* text) = 0;
-	};
+	//class IDisplay
+	//{
+	//public:
+	//	virtual void		SetFuncLabel(GCKey key, const char* label) = 0;
+	//	virtual void		DisplayText(int row, int col, const char* text) = 0;
+	//};
 }
