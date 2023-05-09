efd3a615d36ffe07d0901b7aad07aa766be5e903
Blake Christensen <bmcdad.christensen@gmail.com>
2023-05-08 20:27:09 -0600

    Initial commit.  Clean up existing mesh and texture issues.  Add 2D scene in blender file.

--- Orbiter/Orbitersdk/samples/SR71R/SR71Vessel_clbk.cpp
+++ Orbiter/Orbitersdk/samples/SR71R/SR71Vessel_clbk.cpp
@@ -41,14 +41,14 @@ void SR71Vessel::clbkSetClassCaps(FILEHANDLE cfg)
 	// You use these mesh handles to the AddMesh the meshes and set how they will be viewed.
 	// If you ever need this handle again, call oapiLoadMeshGlobal with the same name and it
 	// we be returned, the mesh will not be reloaded.
-	auto mainGlobalMesh = oapiLoadMeshGlobal(SR71r_MESH_NAME);
+	auto mainGlobalMesh = oapiLoadMeshGlobal(bt_mesh::SR71r::MESH_NAME);
 	mainMeshIndex_ = AddMesh(mainGlobalMesh);
 	SetMeshVisibilityMode(mainMeshIndex_, MESHVIS_EXTERNAL);
     SetMainMeshIndex(mainMeshIndex_);  // The index will be needed when adding animations to the mesh.
 
 	// In case of the VC mesh we hold on to the mesh handle as it will be needed to get the texture
 	// that will be modified for things like MFD font painting.
-	vcMeshHandle_ = oapiLoadMeshGlobal(SR71rVC_MESH_NAME);
+	vcMeshHandle_ = oapiLoadMeshGlobal(bt_mesh::SR71rVC::MESH_NAME);
 	auto idx = AddMesh(vcMeshHandle_);
 	SetMeshVisibilityMode(idx, MESHVIS_VC);
 	SetVCMeshIndex0(idx);
